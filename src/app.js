9848e070a62bcf40f87b418f5d4e79b90df5c1bc
Sheetal Soni <sheetalsoni@Sheetals-MacBook-Air.local>
2021-06-20 19:59:50 +0530

    changes in about page

--- src/app.js
+++ src/app.js
@@ -24,14 +24,14 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather",
-    name: "Andrew Mead",
+    name: "Sheetal Soni",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Me",
-    name: "Andrew Mead",
+    name: "Sheetal Soni",
   });
 });
 
@@ -39,7 +39,7 @@ app.get("/help", (req, res) => {
   res.render("help", {
     helpText: "This is some helpful text.",
     title: "Help",
-    name: "Andrew Mead",
+    name: "Sheetal Soni",
   });
 });
 
@@ -114,7 +114,7 @@ app.get("/products", (req, res) => {
 app.get("/help/*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Sheetal Soni",
     errorMessage: "Help article not found.",
   });
 });
@@ -122,7 +122,7 @@ app.get("/help/*", (req, res) => {
 app.get("*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Sheetal Soni",
     errorMessage: "Page not found.",
   });
 });
